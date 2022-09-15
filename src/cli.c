042cb5e25fc9ed6075aba1201b9c3ac7ae31686a
7Ji <pugokushin@gmail.com>
2022-09-15 12:27:47 +0800

    CLI: WIP

--- src/cli.c
+++ src/cli.c
@@ -91,11 +91,9 @@ cli_describe_options() {
     fprintf(stderr, "CLI describe options: mode %s, operating on %s, content type %s, dry run: %s, reserved gap: %lu (%lf%c), generic gap: %lu (%lf%c), reserved offset: %lu (%lf%c), dtb offset: %lu (%lf%c)\n", cli_mode_strings[cli_options.mode], cli_options.target, cli_content_type_strings[cli_options.content], cli_options.dry_run ? "yes" : "no", cli_options.gap_reserved, gap_reserved, suffix_gap_reserved, cli_options.gap_partition, gap_partition, suffix_gap_partition, cli_options.offset_reserved, offset_reserved, suffix_offset_reserved, cli_options.offset_dtb, offset_dtb, suffix_offset_dtb);
 }
 
-int 
-cli_early_stage(
-    int     argc,
-    char *  argv[]
-){
+static inline
+int
+cli_read(){
     int fd = open(cli_options.target, O_RDONLY);
     if (fd < 0) {
         fputs("CLI early stage: Failed to open target\n", stderr);
@@ -163,19 +161,59 @@ cli_early_stage(
         }
         free(table);
     }
-    for (int i =0; i<argc; ++i) {
-        printf("%d: %s\n", i, argv[i]);
-    }
     close(fd);
     return 0;
 }
 
-int cli_interface(
-    int const   argc,
-    char *      argv[]
+static inline
+int 
+cli_early_stage(
+    int     argc,
+    char *  argv[]
+){
+    int const r = cli_read();
+    for (int i =0; i<argc; ++i) {
+        printf("%d: %s\n", i, argv[i]);
+    }
+    return r;
+}
+
+static inline
+int
+cli_parse_mode(){
+    for (enum cli_modes mode = CLI_MODE_INVALID; mode <= CLI_MODE_ECREATE; ++mode) {
+        if (!strcmp(cli_mode_strings[mode], optarg)) {
+            fprintf(stderr, "CLI interface: Mode is set to %s\n", optarg);
+            cli_options.mode = mode;
+            return 0;
+        }
+    }
+    fprintf(stderr, "CLI interface: Invalid mode %s\n", optarg);
+    return 1;
+}
+
+static inline
+int
+cli_parse_content(){
+    for (enum cli_content_types content = CLI_CONTENT_TYPE_AUTO; content <= CLI_CONTENT_TYPE_DISK; ++content) {
+        if (!strcmp(cli_content_type_strings[content], optarg)) {
+            fprintf(stderr, "CLI interface: Content type is set to %s\n", optarg);
+            cli_options.content = content;
+            return 0;
+        }
+    }
+    fprintf(stderr, "CLI interface: Invalid type %s\n", optarg);
+    return 1;
+}
+
+static inline
+int
+cli_parse_options(
+    int const * const   argc,
+    char *              argv[]
 ){
     int c, option_index = 0;
-    static struct option long_options[] = {
+    struct option const long_options[] = {
         {"version",         no_argument,        NULL,   'v'},
         {"help",            no_argument,        NULL,   'h'},
         {"mode",            required_argument,  NULL,   'm'},
@@ -188,49 +226,25 @@ int cli_interface(
         {"gap-reserved",    required_argument,  NULL,   'r'},
         {NULL,              0,                  NULL,  '\0'}
     };
-    while ((c = getopt_long(argc, argv, "vhm:c:dR:D:p:r:", long_options, &option_index)) != -1) {
+    while ((c = getopt_long(*argc, argv, "vhm:c:dR:D:p:r:", long_options, &option_index)) != -1) {
         switch (c) {
             case 'v':   // version
                 cli_version();
-                return 0;
+                return -1;
             case 'h':   // help
                 cli_version();
                 fputs("\nampart does not provide command-line help message due to its complex syntax, please refer to the project Wiki for documentation:\nhttps://github.com/7Ji/ampart/wiki\n\n", stderr);
-                return 0;
-            case 'm': {   // mode:
-                bool mode_valid = false;
-                for (enum cli_modes mode = CLI_MODE_INVALID; mode <= CLI_MODE_ECREATE; ++mode) {
-                    if (!strcmp(cli_mode_strings[mode], optarg)) {
-                        fprintf(stderr, "CLI interface: Mode is set to %s\n", optarg);
-                        mode_valid = true;
-                        cli_options.mode = mode;
-                        break;
-                    }
-                }
-                if (mode_valid) {
-                    break;
-                } else {
-                    fprintf(stderr, "CLI interface: Invalid mode %s\n", optarg);
+                return -1;
+            case 'm':  // mode:
+                if (cli_parse_mode()) {
                     return 1;
                 }
-            }
-            case 'c': {   // type:
-                bool type_valid = false;
-                for (enum cli_content_types content = CLI_CONTENT_TYPE_AUTO; content <= CLI_CONTENT_TYPE_DISK; ++content) {
-                    if (!strcmp(cli_content_type_strings[content], optarg)) {
-                        fprintf(stderr, "CLI interface: Content type is set to %s\n", optarg);
-                        type_valid = true;
-                        cli_options.content = content;
-                        break;
-                    }
-                }
-                if (type_valid) {
-                    break;
-                } else {
-                    fprintf(stderr, "CLI interface: Invalid type %s\n", optarg);
+                break;
+            case 'c':   // type:
+                if (cli_parse_content()) {
                     return 2;
                 }
-            }    
+                break;
             case 's':   // strict-device
                 fputs("CLI interface: Enabled strict-device, ampart won't try to find the corresponding disk when target is a block device and is not a full eMMC drive\n", stderr);
                 cli_options.strict_device = true;
@@ -256,9 +270,79 @@ int cli_interface(
                 return 3;
         }
     }
+    return 0;
+}
+
+static inline
+int
+cli_find_disk(){
+    char *path_disk = io_find_disk(cli_options.target);
+    if (!path_disk) {
+        fprintf(stderr, "CLI interface: Failed to get the corresponding disk of %s, try to force the target type or enable strict-device mode to disable auto-identification\n", cli_options.target);
+        return 1;
+    }
+    fprintf(stderr, "CLI interface: Operating on '%s' instead, content type is now disk\n", path_disk);
+    free(cli_options.target);
+    cli_options.target = path_disk;
+    cli_options.content = CLI_CONTENT_TYPE_DISK;
+    return 0;
+}
+
+static inline
+int
+cli_options_complete_target_info(){
+    struct io_target_type *target_type = io_identify_target_type(cli_options.target);
+    if (!target_type) {
+        fprintf(stderr, "CLI interface: failed to identify the type of target '%s'\n", cli_options.target);
+        return 1;
+    }
+    cli_options.size = target_type->size;
+    io_describe_target_type(target_type, NULL);
+    bool find_disk = false;
+    if (cli_options.content == CLI_CONTENT_TYPE_AUTO) {
+        fputs("CLI interface: Start auto content type identification, specify target type if you don't want this\n", stderr);
+        switch (target_type->content) {
+            case IO_TARGET_TYPE_CONTENT_DISK:
+                fputs("CLI interface: Content auto identified as whole disk\n", stderr);
+                cli_options.content = CLI_CONTENT_TYPE_DISK;
+                break;
+            case IO_TARGET_TYPE_CONTENT_RESERVED:
+                fputs("CLI interface: Content auto identified as reserved partition\n", stderr);
+                cli_options.content = CLI_CONTENT_TYPE_RESERVED;
+                break;
+            case IO_TARGET_TYPE_CONTENT_DTB:
+                fputs("CLI interface: Content auto identified as DTB partition\n", stderr);
+                cli_options.content = CLI_CONTENT_TYPE_DTB;
+                break;
+            case IO_TARGET_TYPE_CONTENT_UNSUPPORTED:
+                fprintf(stderr, "CLI interface: failed to identify the content type of target '%s', please set its type manually\n", cli_options.target);
+                return 2;
+        }
+        if (target_type->file == IO_TARGET_TYPE_FILE_BLOCKDEVICE && target_type->content != IO_TARGET_TYPE_CONTENT_DISK) {
+            find_disk = true;
+        }
+    } else if (target_type->file == IO_TARGET_TYPE_FILE_BLOCKDEVICE && cli_options.content != CLI_CONTENT_TYPE_DISK) {
+        find_disk = true;
+    }
+    free(target_type);
+    if (!cli_options.strict_device && find_disk) {
+        int r = cli_find_disk();
+        if (r) {
+            return 3;
+        }
+    }
+    return 0;
+}
+
+static inline
+int
+cli_complete_options(
+    int const   argc,
+    char *      argv[]
+){
     if (cli_options.mode == CLI_MODE_INVALID) {
         fputs("CLI interface: Mode not set or invalid, you must specify the mode with --mode [mode] argument\n", stderr);
-        return 4;
+        return 1;
     }
     if (cli_options.dry_run) {
         cli_options.write = CLI_WRITE_NOTHING;
@@ -267,66 +351,40 @@ int cli_interface(
         cli_options.target = strdup(argv[optind++]);
         if (!cli_options.target) {
             fprintf(stderr, "CLI interface: Failed to duplicate target string '%s'\n", argv[optind-1]);
-            return 4;
+            return 2;
         }
         fprintf(stderr, "CLI interface: Operating on target file/block device '%s'\n", cli_options.target);
-        bool find_disk = false;
-        struct io_target_type *target_type = io_identify_target_type(cli_options.target);
-        if (!target_type) {
-            fprintf(stderr, "CLI interface: failed to identify the type of target '%s'\n", cli_options.target);
-            return 5;
-        }
-        cli_options.size = target_type->size;
-        io_describe_target_type(target_type, NULL);
-        if (cli_options.content == CLI_CONTENT_TYPE_AUTO) {
-            fputs("CLI interface: Start auto content type identification, specify target type if you don't want this\n", stderr);
-            switch (target_type->content) {
-                case IO_TARGET_TYPE_CONTENT_DISK:
-                    fputs("CLI interface: Content auto identified as whole disk\n", stderr);
-                    cli_options.content = CLI_CONTENT_TYPE_DISK;
-                    break;
-                case IO_TARGET_TYPE_CONTENT_RESERVED:
-                    fputs("CLI interface: Content auto identified as reserved partition\n", stderr);
-                    cli_options.content = CLI_CONTENT_TYPE_RESERVED;
-                    break;
-                case IO_TARGET_TYPE_CONTENT_DTB:
-                    fputs("CLI interface: Content auto identified as DTB partition\n", stderr);
-                    cli_options.content = CLI_CONTENT_TYPE_DTB;
-                    break;
-                case IO_TARGET_TYPE_CONTENT_UNSUPPORTED:
-                    fprintf(stderr, "CLI interface: failed to identify the content type of target '%s', please set its type manually\n", cli_options.target);
-                    return 6;
-            }
-            if (target_type->file == IO_TARGET_TYPE_FILE_BLOCKDEVICE && target_type->content != IO_TARGET_TYPE_CONTENT_DISK) {
-                find_disk = true;
-            }
-        } else if (target_type->file == IO_TARGET_TYPE_FILE_BLOCKDEVICE && cli_options.content != CLI_CONTENT_TYPE_DISK) {
-            find_disk = true;
-        }
-        free(target_type);
-        if (!cli_options.strict_device && find_disk) {
-            char *path_disk = io_find_disk(cli_options.target);
-            if (!path_disk) {
-                fprintf(stderr, "CLI interface: Failed to get the corresponding disk of %s, try to force the target type or enable strict-device mode to disable auto-identification\n", cli_options.target);
-                return 7;
-            }
-            fprintf(stderr, "CLI interface: Operating on '%s' instead, content type is now disk\n", path_disk);
-            free(cli_options.target);
-            cli_options.target = path_disk;
-            cli_options.content = CLI_CONTENT_TYPE_DISK;
+        int const r = cli_options_complete_target_info();
+        if (r) {
+            return 3;
         }
     } else {
         fputs("CLI interface: Too few arguments, target file/block device must be set as the first non-positional argument\n", stderr);
-        return 8;
+        return 4;
     }
     if (cli_options.content == CLI_CONTENT_TYPE_AUTO) {
         fputs("CLI interface: Content type not identified, give up, try setting it manually\n", stderr);
-        return 9;
+        return 5;
+    }
+    return 0;
+}
+
+int cli_interface(
+    int const   argc,
+    char *      argv[]
+){
+    int r = cli_parse_options(&argc, argv);
+    if (r < 0) {
+        return 0;
+    } else if (r > 0) {
+        return r;
+    }
+    if ((r = cli_complete_options(argc, argv))) {
+        return 10 + r;
     }
     cli_describe_options();
-    int r = cli_early_stage(argc-optind, argv+optind);
-    if (r)  {
-        return 10 + r;
+    if ((r = cli_early_stage(argc-optind, argv+optind))) {
+        return 20 + r;
     }
     return 0;
 }

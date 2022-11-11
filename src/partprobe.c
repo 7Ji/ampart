90b8a51fa5d3d4ab262e8641644deca3d4323795
7Ji <pugokushin@gmail.com>
2022-11-11 11:41:44 +0800

    src/partprobe: removed

--- src/partprobe.c
+++
@@ -1,5 +0,0 @@
-#include "partprobe.h"
-
-#define PARTPROBE_SYS_MMCBLK          "/sys/bus/mmc/drivers/mmcblk"
-#define PARTPROBE_SYS_MMCBLK_UNBIND   SYS_MMCBLK"/unbind"
-#define PARTPROBE_SYS_MMCBLK_BIND     SYS_MMCBLK"/bind"

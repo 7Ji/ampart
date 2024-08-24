bf208d30ccafa3d9c30a20160881d977a980ccbb
Guoxin Pu <pugokushin@gmail.com>
2024-08-24 20:46:45 +0800

    release: 1.4.1
    Force a release for the fixup commit 0fa859806a62e138e4114ae98bcbf56241977127
    ("cli: fix cli_options.target leaking memory")

--- src/version.c
+++ src/version.c
@@ -6,7 +6,7 @@ char const version[] =
 #ifdef VERSION
     VERSION;
 #else
-    "1.3-"
+    "1.4.1-"
     "UNKNOWN-Development-Only!\n"
     "You are seeing this version message because the distributor did not build the binary in the git work tree nor pass the version via VERSION= when running make. This version will NOT be supported because the maintainer won't know which exact version you're using!";
 #endif
